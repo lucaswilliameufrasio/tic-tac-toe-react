7199feb8490668a17e39f0649ceb89fafddda16d
Lucas William <lucaswilliameufrasio@gmail.com>
2020-07-05 02:05:27 -0300

    feat: game itself and start new game function

---
+++ src/components/Game/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Board from "../Board";
+import { calculateWinner } from "../../helpers/calculateWinner";
+
+import styles from "./styles";
+
+function Game() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+
+  const winner = calculateWinner(board);
+
+  function handleClick(index) {
+    const boardCopy = [...board];
+
+    //If user click on occupied square or if game is won, return
+    if (winner || boardCopy[index]) return;
+
+    // Put an X or an O in the clicked square
+    boardCopy[index] = xIsNext ? "X" : "O";
+
+    setBoard(boardCopy);
+    setXisNext(!xIsNext);
+  }
+
+  function jumpTo() {}
+
+  function renderMoves() {
+    return (
+      <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+    );
+  }
+
+  return (
+    <>
+      <Board squares={board} onClick={handleClick} />
+      <div style={styles.winner}>
+        <p>
+          {winner ? `Winner ${winner}` : `Next player ${xIsNext ? "X" : "O"}`}
+        </p>
+        {renderMoves()}
+      </div>
+    </>
+  );
+}
+
+export default Game;
---
+++ src/components/Game/styles.js
@@ -0,0 +1,10 @@
+const styles = {
+    winner: {
+        widht: '280px',
+        margin: '20px auto',
+        display: 'flex',
+        alignItems: 'center',
+    }
+}
+
+export default styles;
